84cd007b47e5058dd97b509b8a57e429ef4d46a4
Admin <codebreaker7@mail.ru>
2018-07-27 14:40:00 +0300

    Initial commint for files

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace FtdiFifo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.identifyButton = new System.Windows.Forms.Button();
+            this.deviveComboBox = new System.Windows.Forms.ComboBox();
+            this.openButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.transmitButton = new System.Windows.Forms.Button();
+            this.measureButton = new System.Windows.Forms.Button();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // identifyButton
+            // 
+            this.identifyButton.Location = new System.Drawing.Point(139, 3);
+            this.identifyButton.Name = "identifyButton";
+            this.identifyButton.Size = new System.Drawing.Size(77, 26);
+            this.identifyButton.TabIndex = 0;
+            this.identifyButton.Text = "Identify";
+            this.identifyButton.UseVisualStyleBackColor = true;
+            this.identifyButton.Click += new System.EventHandler(this.identifyButton_Click);
+            // 
+            // deviveComboBox
+            // 
+            this.deviveComboBox.FormattingEnabled = true;
+            this.deviveComboBox.Location = new System.Drawing.Point(12, 7);
+            this.deviveComboBox.Name = "deviveComboBox";
+            this.deviveComboBox.Size = new System.Drawing.Size(121, 21);
+            this.deviveComboBox.TabIndex = 1;
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(222, 3);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(75, 26);
+            this.openButton.TabIndex = 2;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(222, 35);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // transmitButton
+            // 
+            this.transmitButton.Location = new System.Drawing.Point(141, 35);
+            this.transmitButton.Name = "transmitButton";
+            this.transmitButton.Size = new System.Drawing.Size(75, 23);
+            this.transmitButton.TabIndex = 4;
+            this.transmitButton.Text = "Transmit";
+            this.transmitButton.UseVisualStyleBackColor = true;
+            this.transmitButton.Click += new System.EventHandler(this.transmitButton_Click);
+            // 
+            // measureButton
+            // 
+            this.measureButton.Location = new System.Drawing.Point(141, 64);
+            this.measureButton.Name = "measureButton";
+            this.measureButton.Size = new System.Drawing.Size(75, 23);
+            this.measureButton.TabIndex = 5;
+            this.measureButton.Text = "Measure speed";
+            this.measureButton.UseVisualStyleBackColor = true;
+            this.measureButton.Click += new System.EventHandler(this.measureButton_Click);
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Location = new System.Drawing.Point(230, 70);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(0, 13);
+            this.speedLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 277);
+            this.Controls.Add(this.speedLabel);
+            this.Controls.Add(this.measureButton);
+            this.Controls.Add(this.transmitButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.deviveComboBox);
+            this.Controls.Add(this.identifyButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button identifyButton;
+        private System.Windows.Forms.ComboBox deviveComboBox;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button transmitButton;
+        private System.Windows.Forms.Button measureButton;
+        private System.Windows.Forms.Label speedLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FTD2XX_NET;
+
+namespace FtdiFifo
+{
+    public partial class Form1 : Form
+    {
+        private FTDI ftHandle = new FTDI();
+        private FTDI.FT_DEVICE_INFO_NODE[] deviceInfos = new FTDI.FT_DEVICE_INFO_NODE[2];
+        private string serialNumber = new string(' ', 32);
+        private byte[] data = new byte[128];
+        private byte[] recData = new byte[128];
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 128; i++)
+            {
+                data[i] = (byte)(i % 256);
+            }
+        }
+
+        private void IdentifyDevice()
+        {
+            FTDI.FT_STATUS status;
+            uint devcount = 0;
+            ftHandle.GetNumberOfDevices(ref devcount);
+            if (devcount > 0)
+            {
+                status = ftHandle.GetDeviceList(deviceInfos);
+                if (status == FTDI.FT_STATUS.FT_OK)
+                {
+                    for (int i = 0; i < devcount; i++)
+                    {
+                        deviveComboBox.Items.Add(deviceInfos[i].Description);
+                    }
+                }
+                status = ftHandle.GetSerialNumber(out serialNumber);
+            }
+        }
+
+        private void SetFifoMode()
+        {
+            FTDI.FT_STATUS status;
+            //status = ftHandle.OpenBySerialNumber(serialNumber);
+            status = ftHandle.OpenByIndex(0);
+            if (status != FTDI.FT_STATUS.FT_OK)
+            {
+                MessageBox.Show("Cannot open device");
+                return;
+            }
+            ftHandle.SetBitMode(0xff, 0x00);
+            System.Threading.Thread.Sleep(10);
+            status = ftHandle.SetBitMode(0xff, 0x40);
+            if (status != FTDI.FT_STATUS.FT_OK)
+            {
+                MessageBox.Show("Cannot set status");
+                return;
+            }
+            ftHandle.SetLatency(16);
+            ftHandle.InTransferSize(0x10000);
+            ftHandle.SetFlowControl(FTDI.FT_FLOW_CONTROL.FT_FLOW_RTS_CTS, 0, 0);
+ 
+        }
+
+        private void SendDataOverFifo()
+        {
+            FTDI.FT_STATUS status;
+            uint txQueue = 0;
+            uint rxQueue = 0;
+            // check buffer
+            status = ftHandle.GetTxBytesWaiting(ref txQueue);
+            if (txQueue == 0)
+            {
+                // write data
+                uint written = 0;
+                ftHandle.Write(data, data.Length, ref written);
+                if (written == 0)
+                {
+                    MessageBox.Show("No data have been written");
+                    return;
+                }
+            }
+            // check read buffer
+            status = ftHandle.GetRxBytesAvailable(ref rxQueue);
+            if (rxQueue > 0)
+            {
+                // read data
+                uint read = 0;
+                ftHandle.Read(recData, (uint)rxQueue, ref read);
+                if (read == 0)
+                {
+                    MessageBox.Show("No data have been read from FIFO");
+                    return;
+                }
+            }
+        }
+
+        private void MeasureSpeed()
+        {
+            byte[] masdata = new byte[4096]; // start array 
+            for (int i = 0; i < masdata.Length; i++)
+            {
+                masdata[i] = (byte)(i % 256);
+            }
+            byte[] bufdata = new byte[4096]; // array with intermediate data
+            byte[] resmasdata = new byte[4096]; // array for receiving data
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            for (int i = 0; i < 16; i++)
+            {
+                // write data
+                uint written = 0;
+                ftHandle.Write(masdata, masdata.Length, ref written);
+                if (written != masdata.Length)
+                {
+                    uint startIndex = written;
+                    uint copySectionSize = 4096 - written;
+                    System.Array.Copy(masdata, startIndex, bufdata, 0, copySectionSize);
+                    ftHandle.Write(bufdata, (int)copySectionSize, ref written);
+                    while (copySectionSize != written)
+                    {
+                        startIndex = written;
+                        copySectionSize = copySectionSize - written;
+                        System.Array.Copy(bufdata, startIndex, bufdata, 0, copySectionSize);
+                        ftHandle.Write(bufdata, (int)copySectionSize, ref written);
+                    }
+                }
+                // read data
+                uint available = 0;
+                uint read = 0;
+                ftHandle.GetRxBytesAvailable(ref available);
+                while (available != 0)
+                {
+                    ftHandle.Read(resmasdata, available, ref read);
+                    if (read != available)
+                    {
+                        // not all data have been read
+                        ftHandle.GetRxBytesAvailable(ref available);
+                    }
+                    ftHandle.GetRxBytesAvailable(ref available);
+                }
+            }
+            var elapsedMs = watch.ElapsedMilliseconds;
+            speedLabel.Text = ((4096 * 16 * 2) / elapsedMs * 1000).ToString() + " bytes/s"; // bytes / (ms*1000)
+        }
+
+        private void identifyButton_Click(object sender, EventArgs e)
+        {
+            IdentifyDevice();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            SetFifoMode();
+        }
+
+        private void transmitButton_Click(object sender, EventArgs e)
+        {
+            SendDataOverFifo();
+        }
+
+        private void measureButton_Click(object sender, EventArgs e)
+        {
+            MeasureSpeed();
+        }
+    }
+}
